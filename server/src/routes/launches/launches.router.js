8ae685256fd38bb3e14fda6a1e42cb89f38c1ed8
Kwabena Agyeman <kwabena@delt.ai>
2022-09-30 22:15:59 +0000

    Added post validations on the post launches request

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const validateData = require('../../../middlewares/validator');
 const launchesRouter = express.Router();
 const {
   httpGetAllLaunches,
   httpAddNewLaunch,
 } = require('./launches.controller');
+const { launchesValidation } = require('./launches.validations');
 
 launchesRouter.get('/', httpGetAllLaunches);
 
-launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.post('/', launchesValidation, validateData, httpAddNewLaunch);
 
 module.exports = launchesRouter;
---
+++ server/src/routes/launches/launches.validations.js
@@ -0,0 +1,18 @@
+const { body } = require('express-validator');
+
+const launchesValidation = [
+  body('mission').isString().notEmpty().withMessage('Mission is required'),
+  body('rocket').isString().notEmpty().withMessage('Rocket is required'),
+  body('destination')
+    .isString()
+    .notEmpty()
+    .withMessage('Destination is required'),
+  body('launchDate')
+    .isString()
+    .notEmpty()
+    .withMessage('Launch-Date is required'),
+];
+
+module.exports = {
+  launchesValidation,
+};
