abcf05cad6784205e7bb5331963286aa308739f6
Kwabena Agyeman <kwabena@delt.ai>
2022-10-31 21:12:05 +0000

    Added Mongo Atlas Cluster and Mongoose package

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  'mongodb+srv://nasa-api:m0v6mrsQus041u3r@cluster0.jvsor.mongodb.net/?retryWrites=true&w=majority';
 
 const { loadPlanetsData } = require('./models/planets.model');
+
+const MongoConnect = async () => {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB connected!!');
+  } catch (error) {
+    console.log('Failed to connect to MongoDB', error);
+  }
+};
 
 const server = http.createServer(app);
 
 const startServer = async () => {
+  await MongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
